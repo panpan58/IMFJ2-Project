23a67008ae70200dd350e889a23e7820d65df5d9
António Branco <ajsbranco15@gmail.com>
2020-06-06 12:24:02 +0100

    Fix wrong input crash's, add refresh screen

--- project.py
+++ project.py
@@ -3,21 +3,41 @@ import pygame.freetype
 import math
 
 
-def InvalidMessage(situation):
+def Messages(situation):
+    
     print("==================================================")
-    print("Invalid Command!")
-    if(situation != 0):
-         print("Please issue a command by the following structure:")
-    else:
-        print("Please issue a Valid Command.")
-    if (situation == 1):
-         print("<set/add> <variable> <quantity>")
-    elif(situation == 2):
-        print("<addforce> <x> <y>")
+    if(situation == 0):
+        print("Please issue a Valid Command.") 
+    elif(situation == 1 or situation == 2):
+        print("Please issue a command by the following structure:")
+
+    if (situation == 1 or situation == 5):
+        if(situation == 5): 
+            print("To change the paramaters of the trajectory:")
+        print("'set <variable> <quantity>'")
+
+    if(situation == 2 or situation == 5):
+        if (situation == 5): 
+            print("To add an external Force to the trajectory:")
+        print("'addforce <x> <y>'")
+
+    if (situation == 5):
+        print("To see a list of current External Forces Type 'forces'")
+        print("To remove an external force type 'removeforce <force list position>'")
+    
+    else: 
+        print("Or type 'help' for the list of commands.")
 
 
 def Draw (velocity, angle, gravity, initX, initY, drawn):
     inAir = 0.0  
+    BLACK = (0 , 0, 0)
+    WHITE = (255, 255, 255)
+
+    screen.fill(BLACK)
+    pygame.draw.line(screen, WHITE, (10, 5),(10, 350,), 1)
+    pygame.draw.line(screen, WHITE, (10, 350),(630, 350,), 1)
+
     while(drawn == False):
         velocityX = velocity*(math.cos(math.radians(angle)))
         velocityY = velocity*(math.sin(math.radians(angle)))
@@ -32,16 +52,13 @@ def Draw (velocity, angle, gravity, initX, initY, drawn):
     pygame.display.flip()
 
 
+
 # Initialize pygame
 pygame.init()
-BLACK = (0 , 0, 0)
-WHITE = (255, 255, 255)
 
-# Window and display surface
+
+# Window
 screen = pygame.display.set_mode((640,360))
-screen.fill(BLACK)
-pygame.draw.line(screen, WHITE, (10, 5),(10, 350,), 1)
-pygame.draw.line(screen, WHITE, (10, 350),(630, 350,), 1)
 
 # Variables
 drawn = False
@@ -60,11 +77,11 @@ pygame.display.flip()
 while(True):
 
     for event in pygame.event.get():
-        #Leave the aplication
+        #Leave the application
         if(event.type == pygame.QUIT):
             exit()
 
-    #Console Message
+    #Console Standart Message
     print("==================================================")
     print("Firing at " + str(angle) + " degrees, at " + str(int(velocity)) + " m/s.")
     print("Viscosity is " + str(viscosity) + ", gravity is " + str(gravity) + ".")
@@ -74,39 +91,47 @@ while(True):
     #Print the trajectory
     Draw(velocity, angle, gravity, initX, initY, drawn)
 
+    #Command the user inputs
     user_input = input().split(' ')
 
-    if(user_input[0] == "set"):
-        if(user_input[1] == "velocity"):
-            velocity = float(user_input[2])
-        elif(user_input[1] == "angle"):
-            angle = float(user_input[2]) 
-        elif(user_input[1] == "gravity"):
-            gravity = -float(user_input[2]) 
-        elif(user_input[1] == "viscosity"):
-            viscosity = float(user_input[2])
+    #Verifies which command the user inputs and if it is correct
+    try:
+        if(user_input[0] == "set"):
+            if(user_input[1] == "velocity"):
+                velocity = float(user_input[2])
+            elif(user_input[1] == "angle"):
+                angle = float(user_input[2]) 
+            elif(user_input[1] == "gravity"):
+                gravity = -float(user_input[2]) 
+            elif(user_input[1] == "viscosity"):
+                viscosity = float(user_input[2])
+            else:
+                Messages(1)
+                
+        elif(user_input[0] == "addforce"):
+            if(len(user_input) == 3):
+                arrayX.append(float(user_input[1]))
+                arrayY.append(float(user_input[2]))
+            else:
+                Messages(2)
+
+        elif(user_input[0] == "forces"):
+            if (len(arrayX) != 0):
+                for i in range(len(arrayX)):
+                    print(str(i) + ". F = (" + str(arrayX[i]) + " " + str(arrayY[i]))
+            else: print("There are no external forces being applied!")
+
+        elif(user_input[0] == "removeforce"):
+            arrayX.pop(int(user_input[1]))
+            arrayY.pop(int(user_input[1]))
+
+        elif(user_input[0] == "help"):
+            Messages(5)
         else:
-            InvalidMessage(1)
-            
-    elif(user_input[0] == "addforce"):
-        if(len(user_input) == 3):
-            arrayX.append(float(user_input[1]))
-            arrayY.append(float(user_input[2]))
-        else:
-            InvalidMessage(2)
-
-    elif(user_input[0] == "forces"):
-        for i in range(len(arrayX)):
-            print(str(i) + ". F = (" + str(arrayX[i]) + " " + str(arrayY[i]))
-
-    elif(user_input[0] == "removeforce"):
-        arrayX.pop(int(user_input[1]))
-        arrayY.pop(int(user_input[1]))
-
-    else:
-        InvalidMessage(0)
+            Messages(0)
+    except:
+        Messages(0)
         
-    #Fill Black then #pygame.draw.line(screen, WHITE, (10, 5),(10, 350,), 1) #pygame.draw.line(screen, WHITE, (10, 350),(630, 350,), 1)
 
 
  
