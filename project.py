5d96e51bf6c9c4ae5097d62239ef7c7f1f2842a1
António Branco <ajsbranco15@gmail.com>
2020-06-16 21:48:55 +0100

    Add Viscosty and change code in general

--- project.py
+++ project.py
@@ -2,7 +2,7 @@ import pygame
 import pygame.freetype
 import math
 
-
+# A method to better acess instructions and messages to the user
 def Messages(situation):
     
     print("==================================================")
@@ -29,34 +29,55 @@ def Messages(situation):
     else: 
         print("Or type 'help' for the list of commands.")
 
-
+# Calculates and draws the trajectory
 def Draw (velocity, angle, gravity, initX, initY, drawn, arrayX, arrayY, mass, viscosity):
-    inAir = 0.0  
+    t = 0.0  
+    timestep = 0.00001
     BLACK = (0 , 0, 0)
     WHITE = (255, 255, 255)
+    forcesX = 0
+    forcesY = 0
+    prevposX = 0
+    prevposY = 0
 
     screen.fill(BLACK)
     pygame.draw.line(screen, WHITE, (10, 5),(10, 350,), 1)
     pygame.draw.line(screen, WHITE, (10, 350),(630, 350,), 1)
 
+    initvelocityX = velocity*(math.cos(math.radians(angle)))
+    initvelocityY = -(velocity*(math.sin(math.radians(angle))))
+
+    # Adds the forces of the array
+    for i in range(len(arrayX)):
+        forcesX += (arrayX[i]/mass)
+        forcesY += (arrayY[i]/mass)
+
+    velocityX = initvelocityX
+    velocityY = initvelocityY
+    # Calculates the position and draws a point in the position
     while(drawn == False):
 
-        velocityX = velocity*(math.cos(math.radians(angle)))
-        velocityY = velocity*(math.sin(math.radians(angle)))
+        t += timestep
+        
+        acelX = (velocityX+forcesX)*(-viscosity)/mass
+        acelY = gravity+((velocityY+forcesY)*viscosity)/mass
 
-        for i in range(len(arrayX)):
-            velocityX = velocityX + (arrayX[i]/mass)
-            velocityY = velocityY + (arrayY[i]/mass)
-            
-        inAir = inAir + 0.001
+        posX = prevposX + (velocityX * (t-timestep)) + ((1/2)*((acelX)*(t-timestep))*(timestep**2))
+        posY = prevposY + (velocityY * (t-timestep)) + ((1/2)*((acelY)*(t-timestep))*(timestep**2))
 
-        posX = initX + (velocityX * inAir)
-        posY = initY - (velocityY * inAir) - ((1/2)*gravity*inAir**2)
-        pygame.draw.circle(screen, WHITE, (int(posX), int(posY)),int(0.5))
-
-        if(posY >= 350 or posX >= 640):
+        velocityX = velocityX+t*acelX
+        velocityY = velocityY-t*acelY
+        
+        prevposX = posX
+        prevposY = posY
+        # Checks the if it is out of screen
+        if(posY+350 > 350 or posX+10 >= 640):
             drawn = True
-    pygame.display.flip()
+        else:
+            pygame.draw.circle(screen, WHITE, (int(posX+10), int(posY+350)),int(0.5))
+
+        
+        pygame.display.flip()
 
 
 
@@ -74,7 +95,7 @@ gravity = -9.8
 angle = 45
 initX = 10
 initY = 350
-viscosity = 1
+viscosity = 0.02
 mass = 1
 arrayX = []
 arrayY = []
@@ -85,7 +106,7 @@ pygame.display.flip()
 while(True):
 
     for event in pygame.event.get():
-        #Leave the application
+        # Leave the application
         if(event.type == pygame.QUIT):
             exit()
 
